f382a72cfcff8086cdd1826bff38e1ec12673bed
Shruti2801 <67599018+Shruti2801@users.noreply.github.com>
2022-01-21 12:31:48 +0530

    Add files via upload

--- collection.java
+++ collection.java
@@ -36,5 +36,68 @@ public class collection {
         System.out.println(list2);
 
 
+        LinkedList<String> ll =new LinkedList<String>();
+        LinkedList<String> ll1 =new LinkedList<String>();
+        ll.add("mango");
+        ll.add("apple");
+
+        ll1.add("grapes");
+        ll1.add("mango");
+        ll1.add("apple");
+
+        ll.addAll(ll1);
+        System.out.println(ll);
+
+        ll.addFirst("kiwi");
+        System.out.println(ll);
+        System.out.println(ll1);
+        ll.removeLast();
+        System.out.println("removig last"+ll);
+        ll.removeFirstOccurrence("mango");
+        System.out.println("removing first occurance of mango"+ll);
+        ll.clear();
+
+
+        HashSet<String> hs=new HashSet<String>();
+        HashMap<Integer,String> hm=new HashMap<Integer,String>();
+        HashMap<Integer,String> map=new HashMap<Integer,String>();
+        hm.put(1,"Mango");
+        hm.put(2,"Apple");
+        hm.put(3,"Banana");
+        hm.put(4,"Grapes");
+
+        map.put(2,"kiwi");
+        System.out.println(hm);
+        System.out.println(map);
+//        Iterator i =hm.entrySet().iterator();
+//        while (i.hasNext()){
+//            System.out.println(i.next());
+//        }
+        hm.putIfAbsent(2,"kiwi");
+
+        for(Map.Entry m : hm.entrySet()){
+            System.out.println(m.getKey()+" "+m.getValue());
+        }
+
+        hm.putAll(map);
+        System.out.println(hm);
+
+
+        hm.remove(3);
+        System.out.println(hm);
+
+        hm.remove(1,"Mango");
+        System.out.println(hm);
+
+        hm.replace(2,"Apple");
+        System.out.println(hm);
+
+        hm.replaceAll((k,v)->"Kiwi");
+//        hm.replaceAll();
+        System.out.println(hm);
+
+
+
+
     }
 }
