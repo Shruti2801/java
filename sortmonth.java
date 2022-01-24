637220199d4fcbc060575ee04618bf02cf43481f
Shruti2801 <67599018+Shruti2801@users.noreply.github.com>
2022-01-24 11:43:51 +0530

    Add files via upload

---
+++ sortmonth.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+
+public class sortmonth{
+    public static void main(String[] args) {
+        List<String> in = new ArrayList<String>();
+        List<Date> out = new ArrayList<Date>();
+
+        in.add("JAN-2014");
+        in.add("JAN-2011");
+        in.add("APR-2013");
+        in.add("NOV-2011");
+
+        try {
+
+            for (String abc1 : in) {
+                Date date = new SimpleDateFormat("MMM-yyyy", Locale.ENGLISH).parse(abc1);
+                out.add(date);
+            }
+
+            System.out.println(out);
+            out.sort(new Comparator<Date>() {
+                public int compare(Date arg0, Date arg1) {
+                    return arg0.compareTo(arg1);
+                }
+            });
+
+
+            for (Date date1 : out) {
+                System.out.println( new SimpleDateFormat("MMM-yyyy", Locale.ENGLISH).format(date1));
+            }
+
+        } catch (ParseException e) {
+
+            e.printStackTrace();
+
+        }
+    }
+    }
+
